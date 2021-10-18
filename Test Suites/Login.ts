c5678ffacccf48ef626320df39b9b59b8b0bd4dd
Anh Nguyen <phuong.anh2216@gmail.com>
2021-10-18 16:08:01 +0700

    Initial

---
+++ Test Suites/Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>af72809d-5fc8-4f3d-881a-8d3f0acdee03</testSuiteGuid>
+   <testCaseLink>
+      <guid>0d422686-ce17-49c8-80d5-0fbf220558b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b267f71-9623-44a6-8661-8c8876755da8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8f8c33c-9a95-4223-9868-0ad680eccfae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0d91754f-bab5-4bff-b789-aa1b4cac3c57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
